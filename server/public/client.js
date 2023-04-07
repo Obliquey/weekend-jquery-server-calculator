fe7f8aa3f7aaa9a6f891b90a8cc3e0abae7cbd3e
Anders Boyum <Acboyum@gmail.com>
2023-04-06 21:24:25 -0500

    added basic HTML framework for the calculator, and began adding the client-side JS

--- server/public/client.js
+++ server/public/client.js
@@ -2,4 +2,8 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log("We're Live!");
+
+    // This will be the function that receives the user input and make a POST req to the server
+    $('#finishers').on('click', createInputObject);
+
 }
