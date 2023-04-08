5df87f46287445f5f2fa4d448c207471b48059e9
Anders Boyum <Acboyum@gmail.com>
2023-04-07 22:30:14 -0500

    added requisite GET and POST reqs/res to transfer computation history. Then built function to post history to the DOM

--- server/public/client.js
+++ server/public/client.js
@@ -12,26 +12,87 @@ function onReady() {
     // click listener for operators
     $('#operatorButtons').on('click', '.operators', operatorFunction)
 
+    // click listener for clearFieldsButton
+    $('#finishers').on('click', '#clearFieldsButton', clearFields)
+
+    // Need function for GET req for calculation history
 }
 let operator;
+
+// function to clear fields when #clearFieldsButton is clicked
+function clearFields() {
+    $('#inputOne').val('');
+    $('#inputTwo').val('');
+}//end clearFields
+
+
 // function to decide the operator
 function operatorFunction() {
     operator = $(this).text();
-}
+}//end operatorFunction
 
-// function to retrieve and compile user input and POST to server
+// function to retrieve and compile user input into object and POST to server
 function createInputObject() {
-    let firstNumber = $('#inputOne').val();
-    let secondNumber = $('#inputTwo').val();
+    // store input values and put into object
+    let value1 = $('#inputOne').val();
+    let value2 = $('#inputTwo').val();
+    let operation = {
+        firstNumber: value1,
+        secondNumber: value2,
+        operator: operator
+    }
+
+    // clear input fields
+    $('#inputOne').val('');
+    $('#inputTwo').val('');
+
+     if (!!value1 && !!value2 && !!operator) {
+        // send object{value1, value2, operator} to server for calculation
+        $.ajax({
+            method: 'POST',
+            data: operation,
+            url: '/calculation',
+        }).then(
+            function (response) {
+            // run function to post calculation history
+                postHistory();
+
+        }).catch(
+            function(error) {
+                console.log("Something went wrong with our POST req to /calculation");
+            }
+        )
+     }
+}//end createInputObject
+
+// function to post GET req, e.g. the returned calculation, to the DOM
+// CALLED BY createInputObject
+function postHistory(){
+    $.ajax({
+        method: 'GET',
+        url: '/history'
+    }).then(
+        function (answerArray) {
+            $('#history').empty();
+            let latestAnswer = answerArray[answerArray.length - 1].answer
+
+            
+            // loop through array, prepending history of calculations to DOM
+            for (let object of answerArray) {
+            $('#history').prepend(`
+                <li class="answer">➡️ ${object.answer}</li>
+            `)
+            }
+        }
+    ).catch(
+        function(error) {
+            console.log("Problem with GET /answer pushing to DOM")
+        }
+    )
+}//end postCalculation
 
 
-    console.log(firstNumber);
-    console.log(operator);
-    console.log(secondNumber);
-}
-
-
-
+// function to event.preventDefault on all UI interactions
 function stopRefresh(event) {
     event.preventDefault();
-}
+}//end stopRefresh
--- server/server.js
+++ server/server.js
@@ -8,7 +8,53 @@ app.use(bodyParser.urlencoded({extended: true}));
 
                 // **TIME TO GET INTO SERIOUS MODE** //
 
+// global variables used in converting user input into sendable answerObject + storying calculation history
+let operation;
+let history = [];
 
+// function to parse objects + calculate
+function calculate(object) {
+    let value1 = Number(object.firstNumber);
+    let value2 = Number(object.secondNumber);
+    let calculation;
+
+    // switch statement to determine which operation to run
+    switch (object.operator) {
+        case '+':
+            calculation = (`${value1} ${object.operator} ${value2} = ${value1 + value2}`);
+            break;
+        case '-':
+            calculation = (`${value1} ${object.operator} ${value2} = ${value1 - value2}`);
+            break;
+        case '*':
+            calculation = (`${value1} ${object.operator} ${value2} = ${value1 * value2}`);
+            break;
+        case '/':
+            calculation = (`${value1} ${object.operator} ${value2} = ${value1 / value2}`);
+            break;
+    }
+
+    return calculation;
+
+} //end calculate
+
+// function to receive POST req including object{value1,value2, operator} for use in calculation 
+app.post('/calculation', (req, res) => {
+    console.log("We're in POST /calculation")
+    operation = req.body;
+    // let answerObject = {answer: `${calculate(operation)}`}
+    history.push({answer: `${calculate(operation)}`});
+    console.log(history);
+    
+    res.sendStatus(201);
+})//end POST /calculation
+
+// server function to process GET req to send answer history back to client-side.
+app.get('/history', (req, res) => {
+    console.log("We're in GET /answer");
+
+    res.send(history);
+})//end GET /answer
 
 
 
